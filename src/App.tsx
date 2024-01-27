517e4a02c6102a5915fc35ef43604aef588b7b7d
Dasha Tarasova <tdariaa@mail.ru>
2024-01-27 05:30:58 +0300

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from "react-router";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={
-        <>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
+      <Route
+        path='/'
+        element={
+          <div className='App'>
+            <header className='App-header'>
+              <img src={logo} className='App-logo' alt='logo' />
               <p>
                 Edit <code>src/App.tsx</code> and save to reload.
               </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
                 Learn React
               </a>
             </header>
           </div>
-        </>} />
-
+        }
+      />
     </Routes>
   );
 }
